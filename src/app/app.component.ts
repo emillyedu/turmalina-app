470274c2ee4114b04ceea9566cf6a80fcd1cc3ed
JoaquimBreno <joaquim.breno@academico.ufpb.br>
2022-02-01 01:13:59 -0300

    3 pontin da desgraça

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  public colorString!: string
   toggleNavbar = true;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private router: Router) { 
   }
+
+  getRoute(){
+    return this.router.events.subscribe(
+      (event: any) => {
+        if (event instanceof NavigationStart) {
+          if(event.url==="/documentation"){
+            this.colorString = "gray";
+          }
+          else{
+            this.colorString = "white";
+          }
+        }
+      }
+    )
+  }
+  ngOnInit(){
+    this.getRoute()
+  }
+
 
   title = 'turmalina';
 }
