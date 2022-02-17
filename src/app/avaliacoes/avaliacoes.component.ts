409474e0bf5b519565ccbe368230a136cce7580d
JoaquimBreno <joaquim.breno@academico.ufpb.br>
2022-02-17 16:17:33 -0300

    Resolvi essa desgraça

--- src/app/avaliacoes/avaliacoes.component.ts
+++ src/app/avaliacoes/avaliacoes.component.ts
@@ -117,7 +117,10 @@ export class AvaliacoesComponent implements OnInit, OnDestroy{
   }
 
   createTable(){
+    this.resultsEvaluation = []
     this.resultsEvaluation = this.mapleafservice.resultsEvaluationId
+
+    this.datesApi = []
     this.datesApi.push([moment(this.resultsEvaluation["start_datetime"]).format("DD/MM/YYYY"), moment(this.resultsEvaluation["end_datetime"]).format("DD/MM/YYYY")])
     let dateFormated = moment(this.datesApi[0][1], 'DD/MM/YYYY').format("YYYY-MM-DD")
     this.relatorioUrl = "https://turmalina-api.herokuapp.com/turmalina_report?management_unit=" + (this.resultsEvaluation["management_unit"]["name"]) + "&date=" + dateFormated
