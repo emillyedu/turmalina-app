44a78faac0a0bce529ae35f7eb60b3853d270785
emilly eduarda <emilly.eduarda@academico.ufpb.br>
2022-02-22 11:49:01 -0300

    realmente nunca tem fim

--- src/app/documentation/sandbox/sandbox.component.ts
+++ src/app/documentation/sandbox/sandbox.component.ts
@@ -12,6 +12,16 @@ export class SandboxComponent {
 
     instructions = `
     # Sandbox
+
+    ## O que é?
+    O sandbox é um ambiente de teste, destinado aos desenvolvedores de sites de municípios, para verificarem se estão atigindo os parâmetros avaliativos do sistema turmalina.
+
+    ## Como funciona?
+    O usuário com permissão, conseguirá acessar o formulário, onde inserirá uma lista de URLs de páginas de municípios, separadas por vírgula. Além disso, poderá inserir o seu email, para que seja possível receber o resultado do teste.
+    
+    <div class="img-sandbox">
+        <img src="../../assets/images/md/sandbox.jpeg" alt="Página inicial ambiente de testes"/>
+    <div>
     `
 
 }
